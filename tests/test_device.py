4d3afbc8620e85d7f973b4209d3549cbd3e9889e
Guido Schmitz <guido.schmitz@fedaix.de>
2020-08-25 22:23:49 +0200

    Add testcase for _get_plcnet_info

--- tests/test_device.py
+++ tests/test_device.py
@@ -2,9 +2,10 @@ from datetime import date
 from unittest.mock import patch
 
 import pytest
-
 from asynctest import CoroutineMock
+
 from devolo_plc_api.device_api.deviceapi import DeviceApi
+from devolo_plc_api.plcnet_api.plcnetapi import PlcNetApi
 
 
 class TestDevice:
@@ -21,3 +22,14 @@ class TestDevice:
             assert mock_device.mt_number == device_info['MT']
             assert mock_device.product == device_info['Product']
             assert type(mock_device.device) == DeviceApi
+
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("mock_plcnet_api")
+    async def test___get_plcnet_info(self, mock_device):
+        with patch("devolo_plc_api.device.Device._get_zeroconf_info", new=CoroutineMock()):
+            device_info = self.device_info['_dvl-plcnetapi._tcp.local.']
+            await mock_device._get_plcnet_info()
+
+            assert mock_device.mac == device_info['PlcMacAddress']
+            assert mock_device.technology == device_info['PlcTechnology']
+            assert type(mock_device.plcnet) == PlcNetApi
