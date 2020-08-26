c9f9180ff38240a9caa6038bd5ec6194b19d94d2
Markus Bong <Markus.Bong@devolo.de>
2020-08-26 08:54:15 +0200

    add credentials to calls

--- devolo_plc_api/clients/protobuf.py
+++ devolo_plc_api/clients/protobuf.py
@@ -1,5 +1,6 @@
 from httpx import DigestAuth
 
+
 class Protobuf:
     """
     Google Protobuf client.
@@ -15,15 +16,20 @@ class Protobuf:
         """ Query URL asynchronously. """
         url = f"{self.url}{sub_url}"
         self._logger.debug(f"Calling {url}")
-        return await self._session.get(url)
+        return await self._session.get(url, auth=DigestAuth(self._user, self._password))
 
     async def async_post(self, sub_url, data):
         url = f"{self.url}{sub_url}"
         self._logger.debug(f"Calling {url}")
-        return await self._session.post(url, auth=DigestAuth("USER", "PASSWORD"), data=data)
+        return await self._session.post(url, auth=DigestAuth(self._user, self._password), data=data)
 
     def get(self, sub_url):
         """ Query URL synchronously. """
         url = f"{self.url}{sub_url}"
         self._logger.debug(f"Calling {url}")
-        return self._session.get(url)
+        return self._session.get(url, auth=DigestAuth(self._user, self._password))
+
+    def post(self, sub_url, data):
+        url = f"{self.url}{sub_url}"
+        self._logger.debug(f"Calling {url}")
+        return self._session.post(url, auth=DigestAuth(self._user, self._password), data=data)
