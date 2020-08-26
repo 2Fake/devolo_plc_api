6b0de4d08f393127c5f647fad06157a4a2da135c
Guido Schmitz <guido.schmitz@fedaix.de>
2020-08-26 21:33:05 +0200

    Add some missing sync methods

--- devolo_plc_api/device_api/deviceapi.py
+++ devolo_plc_api/device_api/deviceapi.py
@@ -2,7 +2,6 @@ import logging
 from typing import Callable
 
 from httpx import Client
-from google.protobuf.json_format import MessageToDict
 
 from ..clients.protobuf import Protobuf
 from ..exceptions.feature import FeatureNotSupported
@@ -44,42 +43,71 @@ class DeviceApi(Protobuf):
             return wrapper
         return feature_decorator
 
+
     @_feature("led")
     async def async_get_led_setting(self):
         """ Get LED setting asynchronously. """
         led_setting = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsGet()
-        response = await self.async_get("LedSettingsGet")
+        response = await self._async_get("LedSettingsGet")
         led_setting.ParseFromString(await response.aread())
-        return MessageToDict(message=led_setting, including_default_value_fields=True, preserving_proto_field_name=True)
+        return self._message_to_dict(message=led_setting)
 
     @_feature("led")
-    async def async_set_led_setting(self, enable: bool):
+    def get_led_setting(self):
+        """ Get LED setting synchronously. """
+        led_setting = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsGet()
+        response = self._get("LedSettingsGet")
+        led_setting.ParseFromString(response.read())
+        return self._message_to_dict(message=led_setting)
+
+    @_feature("led")
+    async def async_set_led_setting(self, enable: bool) -> bool:
+        """ Set LED setting asynchronously. """
         led_setting = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsSet()
         led_setting.state = int(not enable)
-        response = await self.async_post("LedSettingsSet", data=led_setting.SerializeToString())
-        r = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsSetResponse()
-        r.ParseFromString(await response.aread())
-        return MessageToDict(message=r, including_default_value_fields=True, preserving_proto_field_name=True)
+        query = await self._async_post("LedSettingsSet", data=led_setting.SerializeToString())
+        response = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsSetResponse()
+        response.ParseFromString(await query.aread())
+        return True if response.result == 0 else False
+
+    @_feature("led")
+    def set_led_setting(self, enable: bool) -> bool:
+        """ Set LED setting synchronously. """
+        led_setting = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsSet()
+        led_setting.state = int(not enable)
+        query = self._post("LedSettingsSet", data=led_setting.SerializeToString())
+        response = devolo_idl_proto_deviceapi_ledsettings_pb2.LedSettingsSetResponse()
+        response.ParseFromString(query.read())
+        return True if response.result == 0 else False
 
     @_feature("wifi1")
-    async def async_get_wifi_connected_station(self):
+    async def async_get_wifi_connected_station(self) -> dict:
+        """ Get wifi stations connected to the device asynchronously. """
         wifi_connected_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiConnectedStationsGet()
-        response = await self.async_get("WifiConnectedStationsGet")
+        response = await self._async_get("WifiConnectedStationsGet")
         wifi_connected_proto.ParseFromString(await response.aread())
-        return wifi_connected_proto
+        return self._message_to_dict(wifi_connected_proto)
+
+    @_feature("wifi1")
+    def get_wifi_connected_station(self) -> dict:
+        """ Get wifi stations connected to the device synchronously. """
+        wifi_connected_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiConnectedStationsGet()
+        response = self._get("WifiConnectedStationsGet")
+        wifi_connected_proto.ParseFromString(response.read())
+        return self._message_to_dict(wifi_connected_proto)
 
     @_feature("wifi1")
     async def async_get_wifi_neighbor_access_points(self):
         # TODO: Why does WifiNeighborAPsGet send a ReadTimeout
         wifi_neighbor_aps = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiNeighborAPsGet()
-        response = await self.async_get("WifiNeighborAPsGet")
+        response = await self._async_get("WifiNeighborAPsGet")
         wifi_neighbor_aps.ParseFromString(await response.aread())
         return wifi_neighbor_aps
 
     @_feature("wifi1")
     async def async_get_wifi_repeated_access_points(self):
         wifi_connected_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiRepeatedAPsGet()
-        response = await self.async_get("WifiRepeatedAPsGet")
+        response = await self._async_get("WifiRepeatedAPsGet")
         wifi_connected_proto.ParseFromString(await response.aread())
         return wifi_connected_proto
 
@@ -88,34 +116,35 @@ class DeviceApi(Protobuf):
         """ Get details about wifi guest access asynchronously. """
         self._logger.debug("Getting wifi guest access")
         wifi_guest_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessGet()
-        response = await self.async_get("WifiGuestAccessGet")
+        response = await self._async_get("WifiGuestAccessGet")
         wifi_guest_proto.ParseFromString(await response.aread())
-        return MessageToDict(wifi_guest_proto)
+        return self._message_to_dict(wifi_guest_proto)
 
     @_feature("wifi1")
     def get_wifi_guest_access(self) -> dict:
         """ Get details about wifi guest access synchronously. """
         self._logger.debug("Getting wifi guest access")
         wifi_guest_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessGet()
-        response = self.get("WifiGuestAccessGet")
+        response = self._get("WifiGuestAccessGet")
         wifi_guest_proto.ParseFromString(response.read())
-        return wifi_guest_proto
+        return self._message_to_dict(wifi_guest_proto)
 
     @_feature("wifi1")
-    async def async_set_wifi_guest_access(self, enable) -> dict:
+    async def async_set_wifi_guest_access(self, enable: bool) -> bool:
         """ Enable wifi guest access asynchronously. """
         wifi_guest_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessSet()
         wifi_guest_proto.enable = enable
-        response = await self.async_post("WifiGuestAccessSet", data=wifi_guest_proto.SerializeToString())
-        r = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessSetResponse()
-        r.ParseFromString(await response.aread())
-        return MessageToDict(message=r, including_default_value_fields=True, preserving_proto_field_name=True)
+        query = await self._async_post("WifiGuestAccessSet", data=wifi_guest_proto.SerializeToString())
+        response = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessSetResponse()
+        response.ParseFromString(await query.aread())
+        return True if response.result == 0 else False
 
     @_feature("wifi1")
-    def set_wifi_guest_access(self, enable) -> dict:
+    def set_wifi_guest_access(self, enable: bool) -> bool:
         """ Enable wifi guest access synchronously. """
         wifi_guest_proto = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessSet()
         wifi_guest_proto.enable = enable
-        response = self.post("WifiGuestAccessSet", data=wifi_guest_proto.SerializeToString())
-        wifi_guest_proto.ParseFromString(response.read())
-        return wifi_guest_proto
+        query = self._post("WifiGuestAccessSet", data=wifi_guest_proto.SerializeToString())
+        response = devolo_idl_proto_deviceapi_wifinetwork_pb2.WifiGuestAccessSetResponse()
+        response.ParseFromString(query.read())
+        return True if response.result == 0 else False
