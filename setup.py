343a3b4c850ae49cf056c37d7aa2ea9e3961ce98
Guido Schmitz <guido.schmitz@fedaix.de>
2020-10-20 12:44:44 +0200

    Exclude tests from packages

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/2Fake/devolo_plc_api",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests*",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
