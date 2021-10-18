00dbe04e964e69b02fe6139d4e2836dfecaf0fd0
Guido Schmitz <guido.schmitz@fedaix.de>
2021-10-18 17:55:34 +0200

    Fix tests for Python 3.10

--- tests/test_device.py
+++ tests/test_device.py
@@ -54,10 +54,9 @@ class TestDevice:
             assert spy_connect.call_count == 1
 
     @pytest.mark.asyncio
-    async def test_async_disconnect(self, mocker, mock_device):
-        spy_zeroconf = mocker.spy(mock_device._zeroconf, "close")
+    async def test_async_disconnect(self, mock_device):
         await mock_device.async_disconnect()
-        assert spy_zeroconf.call_count == 1
+        assert mock_device._zeroconf.close.call_count == 1
         assert mock_device._session.aclose.call_count == 1
         assert not mock_device._connected
 
