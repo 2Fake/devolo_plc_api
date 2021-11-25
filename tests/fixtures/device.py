57e3343fdbe25e8571272e25e55fb65249b7c985
Guido Schmitz <guido.schmitz@fedaix.de>
2021-11-25 20:17:07 +0100

    Remove useless patch

--- tests/fixtures/device.py
+++ tests/fixtures/device.py
@@ -1,5 +1,4 @@
 from copy import deepcopy
-from sys import platform
 from typing import Generator, Type
 from unittest.mock import AsyncMock, patch
 
@@ -12,13 +11,11 @@ from ..mocks.mock_zeroconf import MockServiceBrowser
 
 @pytest.fixture()
 def mock_device(request: pytest.FixtureRequest) -> Generator[Device, None, None]:
-    selector = "windows_events._WindowsSelectorEventLoop" if platform == "win32" else "unix_events._UnixSelectorEventLoop"
-    with patch(f"devolo_plc_api.device.asyncio.{selector}.close"):
-        device = Device(ip=request.cls.ip)
-        device._info = deepcopy(request.cls.device_info)
-        device._session = AsyncMock()
-        device._zeroconf = AsyncMock()
-        yield device
+    device = Device(ip=request.cls.ip)
+    device._info = deepcopy(request.cls.device_info)
+    device._session = AsyncMock()
+    device._zeroconf = AsyncMock()
+    yield device
 
 
 @pytest.fixture()
