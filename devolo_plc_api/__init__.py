fe89edf1ef92dd11be61f03756ac79d96dc0a139
Guido Schmitz <guido.schmitz@fedaix.de>
2022-04-21 15:54:52 +0200

    Fix scm

--- devolo_plc_api/__init__.py
+++ devolo_plc_api/__init__.py
@@ -1,8 +1,7 @@
 """devolo PLC API"""
-from importlib.metadata import PackageNotFoundError, version
-
 try:
-    __version__ = version("devolo_plc_api")
-except PackageNotFoundError:
-    # package is not installed - e.g. pulled and run locally
+    from setuptools_scm import get_version
+
+    __version__ = get_version()
+except ImportError:
     __version__ = "0.0.0"
