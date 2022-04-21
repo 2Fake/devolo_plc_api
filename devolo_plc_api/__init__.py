9fb2f1e7a6a377ed0330926a7eea9642a59fdaa8
Guido Schmitz <guido.schmitz@fedaix.de>
2022-04-21 22:32:56 +0200

    Add fallback_version

--- devolo_plc_api/__init__.py
+++ devolo_plc_api/__init__.py
@@ -2,6 +2,6 @@
 try:
     from setuptools_scm import get_version
 
-    __version__ = get_version()
+    __version__ = get_version(fallback_version="0.0.0")
 except ImportError:
     __version__ = "0.0.0"
