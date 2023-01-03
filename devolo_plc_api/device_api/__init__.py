dee4617da680685a35ac48051d1aecd0456b7764
Guido Schmitz <Shutgun@users.noreply.github.com>
2023-01-03 23:21:36 +0100

    Usage improvements (#101)

--- devolo_plc_api/device_api/__init__.py
+++ devolo_plc_api/device_api/__init__.py
@@ -1,9 +1,21 @@
 """The devolo device API."""
 from .deviceapi import DeviceApi
 from .updatefirmware_pb2 import UpdateFirmwareCheck
-from .wifinetwork_pb2 import WifiConnectedStationsGet, WifiGuestAccessGet, WifiNeighborAPsGet, WifiRepeatedAPsGet
+from .wifinetwork_pb2 import (
+    WIFI_BAND_2G,
+    WIFI_BAND_5G,
+    WIFI_VAP_GUEST_AP,
+    WIFI_VAP_MAIN_AP,
+    WIFI_VAP_STATION,
+    WifiConnectedStationsGet,
+    WifiGuestAccessGet,
+    WifiNeighborAPsGet,
+    WifiRepeatedAPsGet,
+)
 
 SERVICE_TYPE = "_dvl-deviceapi._tcp.local."
+UPDATE_AVAILABLE = UpdateFirmwareCheck.UPDATE_AVAILABLE
+UPDATE_NOT_AVAILABLE = UpdateFirmwareCheck.UPDATE_NOT_AVAILABLE
 
 RepeatedAPInfo = WifiRepeatedAPsGet.RepeatedAPInfo
 ConnectedStationInfo = WifiConnectedStationsGet.ConnectedStationInfo
@@ -14,6 +26,13 @@ __all__ = [
     "DeviceApi",
     "NeighborAPInfo",
     "RepeatedAPInfo",
-    "UpdateFirmwareCheck",
     "WifiGuestAccessGet",
+    "SERVICE_TYPE",
+    "UPDATE_AVAILABLE",
+    "UPDATE_NOT_AVAILABLE",
+    "WIFI_BAND_2G",
+    "WIFI_BAND_5G",
+    "WIFI_VAP_GUEST_AP",
+    "WIFI_VAP_MAIN_AP",
+    "WIFI_VAP_STATION",
 ]
