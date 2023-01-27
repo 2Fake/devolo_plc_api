769b7c1a7cceb4a305d56cb1acbf9ad7d4faaeb6
Guido Schmitz <guido.schmitz@fedaix.de>
2023-01-27 20:29:12 +0100

    Reduce code duplication

--- devolo_plc_api/exceptions/device.py
+++ devolo_plc_api/exceptions/device.py
@@ -4,10 +4,22 @@
 class DeviceNotFound(Exception):
     """The device was not found."""
 
+    def __init__(self, ip: str) -> None:
+        """Initialize error."""
+        super().__init__(f"The device {ip} did not answer.")
+
 
 class DeviceUnavailable(Exception):
     """The device is not available, e.g. in standby."""
 
+    def __init__(self) -> None:
+        """Initialize error."""
+        super().__init__("The device is currently not available. Maybe on standby?")
+
 
 class DevicePasswordProtected(Exception):
     """The device is password protected."""
+
+    def __init__(self) -> None:
+        """Initialize error."""
+        super().__init__("The used password is wrong.")
